ac4e67d911d84fe1e8908277fc70d56abfe85bdf
Simon Opstrup Drue <simon.drue@clin.au.dk>
2022-02-04 10:22:47 +0100

    Adding load_BAM function + test

--- tests/testthat/test-load_BAM.R
+++ tests/testthat/test-load_BAM.R
@@ -1,4 +1,14 @@
 test_that("Load simple bam", {
-  small_sam_file <- system.file("extdata", "small_sam_file.bam", package="dreams")
-  load_BAM(small_sam_file)
+  small_sam_file <- system.file("extdata", "small_sam_file.bam", package = "dreams")
+  bam_df = load_BAM(small_sam_file)
+
+  # Check number of reads
+  expect_equal(nrow(bam_df), 2)
+
+  # Check columns
+  expected_standard_columns = c("qname","rname", "flag","strand","pos","qwidth","mapq","cigar","mpos","isize","seq","qual","MD","chr")
+  expected_umi_columns = c("ce","cd","cE","cD","chr")
+
+  expect_true(all(expected_standard_columns %in% colnames(bam_df)))
+  expect_true(all(expected_umi_columns %in% colnames(bam_df)))
 })
