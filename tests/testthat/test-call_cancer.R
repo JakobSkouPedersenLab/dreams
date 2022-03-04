3a49f6d0ffe0413d0af8487b4501f794fd1d0b6d
Simon Opstrup Drue <simon.drue@clin.au.dk>
2022-03-04 13:31:10 +0100

    Started testing call_cancer

--- tests/testthat/test-call_cancer.R
+++ tests/testthat/test-call_cancer.R
@@ -1,15 +1,3 @@
-test_that("test ...", {
-  mutations_df <-
-    data.frame(
-      CHROM = "chr1",
-      POS = 10,
-      REF = "T",
-      alt = "A"
-    )
-
-  call_cancer(mutations_df = mutations_df, reads = data.frame(), model = NULL, beta = NULL)
-})
-
 test_that("Invalid mutations_df", {
   mutations_no_alt_df <-
     data.frame(
@@ -40,4 +28,18 @@ test_that("Empty mutations_df", {
   expect_equal(res$cancer_info$total_coverage, 0)
   expect_equal(res$cancer_info$p_val, 1)
   expect_equal(res$cancer_info$EM_converged, NA)
+})
+
+test_that("test ...", {
+  mutations_df <-
+    data.frame(
+      CHROM = "chr1",
+      POS = 10,
+      REF = "T",
+      alt = "A"
+    )
+
+  model = system.file("extdata", "mini_example.bam", package = "dreams")
+
+  call_cancer(mutations_df = mutations_df, reads = data.frame(), model = NULL, beta = NULL)
 })
