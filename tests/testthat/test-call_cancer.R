d4dd3d7817bab85beb2b1e3c27359d75eae13f6b
Simon Opstrup Drue <simon.drue@clin.au.dk>
2022-03-18 08:16:46 +0100

    Refactored predict_error_rates

--- tests/testthat/test-call_cancer.R
+++ tests/testthat/test-call_cancer.R
@@ -211,6 +211,50 @@ test_that("Two mutations - one read in another position", {
   expect_equal(res$mutation_info$P_mut_is_present, c(0, 0))
 })
 
+test_that("Adding mutation with no data should not change output", {
+  one_mutations_df <-
+    data.frame(
+      CHROM = "chr1",
+      POS = 10,
+      REF = "A",
+      ALT = "T"
+    )
+
+  two_mutations_df <-
+    bind_rows(
+      one_mutations_df,
+      data.frame(
+        CHROM = "chr2",
+        POS = 13,
+        REF = "A",
+        ALT = "T"
+      )
+    )
+
+  # Reads on Mut 1,2 - No reads on Mut 3 - And random read
+  some_read_positions_df <-
+    data.frame(
+      chr = "chr1",
+      genomic_pos = 10,
+      ref = "A",
+      obs =
+        c(
+          "T", "A", "A", "A", "A", "A"
+        )
+    )
+
+  model_path <- system.file("extdata", "model_test.h5", package = "dreams")
+  model <- keras::load_model_hdf5(model_path)
+
+  res_one_mutations <- call_cancer(mutations_df = one_mutations_df, read_positions_df = some_read_positions_df, model = model, beta = 0.01)
+  res_two_mutations <- call_cancer(mutations_df = two_mutations_df, read_positions_df = some_read_positions_df, model = model, beta = 0.01)
+
+  # Is cancer results the same?
+  expect_equal(res_one_mutations$cancer_info$tf_est, res_two_mutations$cancer_info$tf_est)
+  expect_equal(res_one_mutations$cancer_info$r_est, res_two_mutations$cancer_info$r_est)
+  expect_equal(res_one_mutations$cancer_info$p_val, res_two_mutations$cancer_info$p_val)
+})
+
 test_that("Small example", {
   three_mutations_df <-
     data.frame(
@@ -258,48 +302,4 @@ test_that("Small example", {
   expect_equal(nrow(res$mutation_info), 3)
   expect_equal(res$mutation_info$count, c(1, 0, 0))
   expect_equal(res$mutation_info$coverage, c(2, 3, 0))
-})
-
-test_that("Adding mutation with no data should not change output", {
-  one_mutations_df <-
-    data.frame(
-      CHROM = "chr1",
-      POS = 10,
-      REF = "A",
-      ALT = "T"
-    )
-
-  two_mutations_df <-
-    bind_rows(
-      one_mutations_df,
-      data.frame(
-        CHROM = "chr2",
-        POS = 13,
-        REF = "A",
-        ALT = "T"
-      )
-    )
-
-  # Reads on Mut 1,2 - No reads on Mut 3 - And random read
-  some_read_positions_df <-
-    data.frame(
-      chr = "chr1",
-      genomic_pos = 10,
-      ref = "A",
-      obs =
-        c(
-          "T", "A", "A", "A", "A", "A"
-        )
-    )
-
-  model_path <- system.file("extdata", "model_test.h5", package = "dreams")
-  model <- keras::load_model_hdf5(model_path)
-
-  res_one_mutations <- call_cancer(mutations_df = one_mutations_df, read_positions_df = some_read_positions_df, model = model, beta = 0.01)
-  res_two_mutations <- call_cancer(mutations_df = two_mutations_df, read_positions_df = some_read_positions_df, model = model, beta = 0.01)
-
-  # Is cancer results the same?
-  expect_equal(res_one_mutations$cancer_info$tf_est, res_two_mutations$cancer_info$tf_est)
-  expect_equal(res_one_mutations$cancer_info$r_est, res_two_mutations$cancer_info$r_est)
-  expect_equal(res_one_mutations$cancer_info$p_val, res_two_mutations$cancer_info$p_val)
 })
