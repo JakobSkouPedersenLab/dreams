fccfa9405deaec23a3e714306fe4a915af99ef33
Mikkel H Christensen <mikkel@hovden.dk>
2022-04-19 09:08:39 +0200

    fixed queue

--- R/call_mutations.R
+++ R/call_mutations.R
@@ -63,20 +63,22 @@ dreams_vc <- function(mutations_df, bam_file_path, reference_path, model,
 
   for (q in queue) {
 
+    print (q)
+
     print (q$chr)
-    print (q$genomic_pos)
+    print (q$pos)
 
     # Get read positions
     read_positions_df <- get_read_positions_from_BAM(
       bam_file_path = bam_file_path,
       chr = q$chr,
-      genomic_pos = q$genomic_pos,
+      genomic_pos = q$pos,
       reference_path
     )
 
     current_mutations <- mutations_df %>% filter(
       chr %in% q$chr,
-      genomic_pos %in% q$genomic_pos
+      genomic_pos %in% q$pos
     )
 
     # Call mutations
