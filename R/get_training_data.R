f1069d7934fdf1f0963376f2416ea8f8f6537311
Mikkel H Christensen <mikkel@hovden.dk>
2022-08-09 13:50:03 +0200

    remove N's

--- R/get_training_data.R
+++ R/get_training_data.R
@@ -187,7 +187,7 @@ filter_mismatch_positions <- function(read_positions, bam_file, mm_rate_max = 1,
 
   # Filter heterozygote positions
 
-  read_positions_summarized <- read_positions %>%
+  read_positions_summarized <- read_positions_filtered %>%
     group_by(.data$chr, .data$genomic_pos) %>%
     summarize(n_mismatches = sum(obs != ref)) %>%
     ungroup()
