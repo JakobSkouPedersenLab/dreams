51bfe6a70de7dcd42ca515938f772fe001e49e8f
Mikkel H Christensen <mikkel@hovden.dk>
2022-10-07 09:04:50 +0200

    prior_error

--- R/train_model.R
+++ R/train_model.R
@@ -133,7 +133,9 @@ prepare_input_layer <- function(training_data_features, ctx3_embed_dim) {
       "umi_errors",
       "local_complexity_1",
       "local_complexity_2",
-      "n_other_errors"
+      "n_other_errors",
+      "prior_error"
+
     )
   all_categorical_variables <-
     c(
@@ -142,7 +144,8 @@ prepare_input_layer <- function(training_data_features, ctx3_embed_dim) {
       "first_in_pair",
       "ctx_minus1",
       "ctx_plus1",
-      "chr"
+      "chr",
+      "genomic_pos"
     )
   all_embedded_variables_ctx3 <-
     c(
