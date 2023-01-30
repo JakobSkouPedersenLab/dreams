c5d3fbc292473f29a4f069404237dfa8fa463f9a
Mikkel H Christensen <mikkel@hovden.dk>
2023-01-30 14:34:10 +0100

    fix q

--- R/extract_features_from_bam.R
+++ R/extract_features_from_bam.R
@@ -120,7 +120,7 @@ extract_features_from_bam <- function(bam_df, reference_path,
         n_deletions_in_read = str_count(.data$cigar, "D"),
         q = utf8ToInt(substring(.data$qual, .data$pos_idx, .data$pos_idx))-33,
         q1 = substring(.data$qual, .data$pos_idx, .data$pos_idx),
-        q2 = utf8ToInt(q),
+        q2 = utf8ToInt(q1),
         q3 = q2 - 33
       ) %>%
       # TODO: Move to filter function! Or do before calling this function!
@@ -133,7 +133,7 @@ extract_features_from_bam <- function(bam_df, reference_path,
         "strand", "first_in_pair", "read_index", "fragment_size",
         "ctx_minus1", "ctx_plus1", "trinucleotide_ctx", "context11",
         "local_complexity_1", "local_complexity_2", "local_GC",
-        "n_other_errors", "n_insertions_in_read", "n_deletions_in_read", "seq_length", "mapq", "q", "q2", "qual"
+        "n_other_errors", "n_insertions_in_read", "n_deletions_in_read", "seq_length", "mapq", "q","q1", "q2", "q3", "qual"
       )
 
     # Add UMI features if asked
